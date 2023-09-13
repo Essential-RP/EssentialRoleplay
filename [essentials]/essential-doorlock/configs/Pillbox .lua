3a32a54528422b087f298f8bcb4c4a9ba11d4d43
Essential Roleplay <yonii10x@gmail.com>
2023-09-13 17:10:26 -0400

    Essentials

---
+++ [essentials]/essential-doorlock/configs/Pillbox .lua
@@ -0,0 +1,14 @@
+
+
+-- Ward C created by yax
+Config.DoorList['Pillbox -Ward C'] = {
+    doorType = 'double',
+    doorRate = 1.0,
+    doors = {
+        {objName = -1700911976, objYaw = 249.98275756836, objCoords = vec3(348.433319, -588.744995, 43.433910)},
+        {objName = -434783486, objYaw = 249.98275756836, objCoords = vec3(349.313751, -586.325989, 43.433910)}
+    },
+    authorizedJobs = { ['ambulance'] = 0 },
+    locked = true,
+    distance = 2,
+}
---
+++ [essentials]/essential-doorlock/configs/Vinewood House Test.lua
@@ -0,0 +1,14 @@
+
+
+-- Vine created by yax
+Config.DoorList['Vinewood House Test-Vine'] = {
+    doorRate = 1.0,
+    objYaw = 149.99998474121,
+    locked = true,
+    objName = 308207762,
+    fixText = false,
+    objCoords = vec3(7.518358, 539.526855, 176.177643),
+    distance = 2,
+    authorizedJobs = { ['police'] = 0 },
+    doorType = 'door',
+}
